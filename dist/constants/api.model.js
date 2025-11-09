8dca70283ca24004a2c92f0364e247edfafde000
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-09 18:18:53 +0100

    remove: dist folder

--- dist/constants/api.model.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONTENT_TYPE = void 0;
-exports.CONTENT_TYPE = { 'Content-Type': 'application/json' };
-//# sourceMappingURL=api.model.js.map
